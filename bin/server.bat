9cd190e45b075b321bdf0d60f733c9d600355c38
KRb JILA <krb.jila@gmail.com>
2023-08-11 11:43:21 -0600

    delete everything because julia web dev is too hard

--- bin/server.bat
+++
@@ -1,1 +0,0 @@
-"C:\Users\Cal\AppData\Local\Programs\Julia-1.9.0\bin\julia" --color=yes --depwarn=no --project=@. -q -i -- "%~dp0..\bootstrap.jl" -s=true %*
--- config/initializers/database.jl
+++
@@ -1,8 +0,0 @@
-using Mongoc
-
-client = Mongoc.Client(MongoDB_URI)
-Mongoc.ping(client)
-
-db = client["data"]
-collection = db["shots"]
-bucket = Mongoc.Bucket(db)
