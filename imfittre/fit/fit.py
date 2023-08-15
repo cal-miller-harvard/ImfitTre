937353a18eaea6229dc04bab96ee4010cd4c4c05
KRb JILA <krb.jila@gmail.com>
2023-08-15 15:49:18 -0600

    automatically fit new images!

--- imfittre/fit/fit.py
+++ imfittre/fit/fit.py
@@ -15,9 +15,19 @@ fit_bp = Blueprint(
 
 async def watch_shots():
     """Watches the database for new shots and updates the list of shots."""
-    async with mongo.db.shots.watch() as stream:
+
+    # TODO: This should be an update operation, but it seems it is a replace in the change stream. This works for now, but we should figure out why.
+    pipeline = [{
+            "$match": { 
+                "$and": [
+                    {"operationType": "replace"},
+                    {"fullDocument.images": {"$exists": True}}
+                ]
+            }
+        }]
+    async with mongo.db.shots.watch(pipeline) as stream:
         async for change in stream:
-            pass
+            await fit_shot(change["documentKey"]["_id"], update_db=True)
 
 @fit_bp.before_app_serving
 async def create_fs():
@@ -25,15 +35,8 @@ async def create_fs():
     fs = AsyncIOMotorGridFSBucket(mongo.db)
     app.add_background_task(watch_shots)
 
-@fit_bp.route('/fit', methods=["GET", "POST"])
-async def fit():
-    shot_id = request.args.get('shot_id', None)
-    update_db = request.args.get('update_db', False)
-
-    # if post request, get config from json
-    if request.method == "POST":
-        config = await request.get_json()
-    else:
+async def fit_shot(shot_id, config=None, update_db=False):
+    if config is None:
         config = calibrations.default_fit
 
     images, data = await db.load_images(mongo.db, fs, shot_id)
@@ -46,4 +49,18 @@ async def fit():
         update = {"fit.{}.result".format(k): v  for k, v in result.items()}
         await db.update_shot(mongo.db, shot_id, update)
 
+    return result
+
+@fit_bp.route('/fit', methods=["GET", "POST"])
+async def fit():
+    shot_id = request.args.get('shot_id', None)
+    update_db = request.args.get('update_db', False)
+
+    # if post request, get config from json
+    if request.method == "POST":
+        config = await request.get_json()
+    else:
+        config = calibrations.default_fit
+
+    result = await fit_shot(shot_id, config, update_db)
     return result
